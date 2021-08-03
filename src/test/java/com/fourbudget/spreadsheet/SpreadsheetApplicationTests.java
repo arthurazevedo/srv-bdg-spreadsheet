2ac3457988cc7a794983ff51c2d8a96945eb2b1e
Mateus Aires <mateus.aires@ccc.ufcg.edu.br>
2021-08-03 15:46:16 -0300

    Initializing spring boot project

---
+++ src/test/java/com/fourbudget/spreadsheet/SpreadsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourbudget.spreadsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadsheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
