2a807baeea3beb86a85c03c8b31a2687d55ef931
Mateus Aires <40031590+mateus-aires@users.noreply.github.com>
2021-08-16 16:06:14 -0300

    Update SpreadsheetRepository.java

--- src/main/java/com/fourbudget/spreadsheet/repository/SpreadsheetRepository.java
+++ src/main/java/com/fourbudget/spreadsheet/repository/SpreadsheetRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpreadsheetRepository  extends JpaRepository<Spreadsheet, Long> {
+public interface SpreadsheetRepository extends JpaRepository<Spreadsheet, Long> {
 
 }
