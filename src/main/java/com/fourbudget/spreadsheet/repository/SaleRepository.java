e375c157b14915e9ae90b13ab8608b32b9f97494
Arthur Azevêdo <arthur.stevam.costa@ccc.ufcg.edu.br>
2021-09-20 13:37:58 -0300

    feat(project) Correcao no post project

---
+++ src/main/java/com/fourbudget/spreadsheet/repository/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.fourbudget.spreadsheet.repository;
+
+import com.fourbudget.spreadsheet.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
