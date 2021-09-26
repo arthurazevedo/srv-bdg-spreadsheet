46b29de463fb606fce94d2fa14ff7919ddcb6dbc
Mateus Aires <mateus.aires@virtus.ufcg.edu.br>
2021-09-26 20:34:47 -0300

    Created price validation and getPriceWithDiscount

--- src/main/java/com/fourbudget/spreadsheet/model/Project.java
+++ src/main/java/com/fourbudget/spreadsheet/model/Project.java
@@ -1,6 +1,8 @@
 package com.fourbudget.spreadsheet.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fourbudget.spreadsheet.config.error.MySystemException;
+import com.fourbudget.spreadsheet.util.messages.ErrorMessageProject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,16 +10,9 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.http.HttpStatus;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
@@ -30,41 +25,49 @@ import java.util.List;
 @EntityListeners(AuditingEntityListener.class)
 public class Project {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String name;
-
-    private boolean isFinished;
-
-    private String email;
-
-    private Double price;
-
-    private Double discount;
-
-    @Column(nullable = false)
-    private Long userId;
-
     @CreatedDate
     @Column(length = 1000)
     @JsonProperty("created_at")
     protected Date createdAt;
-
     @CreatedBy
     @JsonProperty("created_by")
     protected Long createdBy;
-
-    @OneToMany(targetEntity=Item.class, fetch= FetchType.EAGER)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private boolean isFinished;
+    private String email;
+    private Double price;
+    private Double discount;
+    @Column(nullable = false)
+    private Long userId;
+    @OneToMany(targetEntity = Item.class, fetch = FetchType.EAGER)
     private List<Item> itemsList;
 
     public Project(Long userId, List<Item> itemsList, String email, String name, Double price, Double discount) {
-        this.userId = userId;
         this.itemsList = itemsList;
+        this.validatePrice(price);
+        this.price = price;
+        this.userId = userId;
         this.email = email;
         this.name = name;
-        this.price = price;
         this.discount = discount;
+    }
+
+    public void validatePrice(Double expectedPrice) {
+        Double actualPrice = new Double(0);
+        for (Item item : this.itemsList) {
+            actualPrice += item.getItemPrice();
+        }
+
+        if (!actualPrice.equals(expectedPrice)) {
+            throw new MySystemException(HttpStatus.OK, ErrorMessageProject.ERROR_MESSAGE_NOT_MATCHING_PRICE);
+        }
+    }
+
+    public Double getPriceWithDiscount() {
+        Double priceWithDiscount = this.price * ((100 - this.discount) / 100);
+        return priceWithDiscount;
     }
 }
---
+++ src/main/java/com/fourbudget/spreadsheet/util/messages/ErrorMessageProject.java
@@ -0,0 +1,5 @@
+package com.fourbudget.spreadsheet.util.messages;
+
+public class ErrorMessageProject {
+    public static final String ERROR_MESSAGE_NOT_MATCHING_PRICE = "Given price doesn't match actual total items price";
+}
