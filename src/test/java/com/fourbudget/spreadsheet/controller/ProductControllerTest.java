1dd704d3e15da13e04da62a14258998597f9b1ce
ViniciusFurtado1 <vinicius.furtado.sa@ccc.ufcg.edu.br>
2021-10-02 22:39:49 -0300

    feat(Product) Criado teste de productController e productServices

---
+++ src/test/java/com/fourbudget/spreadsheet/controller/ProductControllerTest.java
@@ -0,0 +1,53 @@
+package com.fourbudget.spreadsheet.controller;
+
+import com.fourbudget.spreadsheet.model.Product;
+import com.fourbudget.spreadsheet.repository.ProductRepository;
+import com.fourbudget.spreadsheet.service.ProductService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductControllerTest {
+
+    private String userId;
+
+    private List<Product> productList;
+
+    private Product product;
+
+    @Mock
+    private ProductController productController;
+
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        productList = new ArrayList<>();
+        product = new Product();
+        userId = "1";
+        product.setUserId(userId);
+        productService = new ProductService(productRepository);
+        productController = new ProductController(productService);
+        productList.add(product);
+    }
+
+    @Test
+    public void getServices() {
+        Mockito.when(productRepository.findByUserId(Mockito.any())).thenReturn(Optional.of(productList));
+        List<Product> returned = productController.getProducts(userId);
+        Assert.assertEquals(1, returned.size());
+        Assert.assertEquals(product, returned.get(0));
+    }
+}
